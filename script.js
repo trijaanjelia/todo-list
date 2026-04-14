22e2fa32fb9d5c954a1295766646c91a1224522e
trijaanjelia <trijaanjelia03@gmail.com>
2026-04-14 17:41:52 +0800

    Membuat To Do List tema langit biru dengan awan dan matahari

--- script.js
+++ script.js
@@ -95,3 +95,17 @@ function filterTask(type) {
         }
     });
 }
+const input = document.getElementById("taskInput");
+const list = document.getElementById("taskList");
+
+function addTask() {
+    if (input.value.trim() === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = input.value;
+
+    li.onclick = () => li.classList.toggle("done");
+
+    list.appendChild(li);
+    input.value = "";
+}
